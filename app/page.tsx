9a64ffa9f015582925a462c5ec910c9af1093720
Joe Meyer <jmeyer@rightpoint.com>
2024-03-19 14:14:30 -0500

    Layout

--- app/page.tsx
+++ app/page.tsx
@@ -206,6 +206,20 @@ export default function Home() {
         </div>
       </div>
 
+      {isAppInstalled && (<button
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      >
+        <h2 className={`mb-3 text-2xl font-semibold`}>
+          Installed{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            ✔
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          Service worker is registered.
+        </p>
+      </button>)}
+
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         {!isPwaSupported && (<button
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -237,20 +251,6 @@ export default function Home() {
               Enable Push Notifications
             </p>
           </button>)}
-
-        {isAppInstalled && (<button
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Installed{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              ✔
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Service worker is registered.
-          </p>
-        </button>)}
 
         {isAppInstalled && isPushGranted && (
           <button
