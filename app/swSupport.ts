c1168c18757312934a5c83c28c72714fa5b0e545
Joe Meyer <jmeyer@rightpoint.com>
2024-04-03 15:02:27 -0500

    fixes check for nextjs build

--- app/swSupport.ts
+++ app/swSupport.ts
@@ -1,11 +1,11 @@
 export const isNotifySupported = () => {
-    return "serviceWorker" in navigator && "Notification" in window && "PushManager" in window;
+    return typeof window !== 'undefined' && "serviceWorker" in navigator && "Notification" in window && "PushManager" in window;
 }
 
 export const isGeoSupported = () => {
-    return "serviceWorker" in navigator && "geolocation" in navigator;
+    return typeof window !== 'undefined' && "serviceWorker" in navigator && "geolocation" in navigator;
 }
 
 export const isStorageSupported = () => {
-    return "serviceWorker" in navigator && "storage" in navigator;
+    return typeof window !== 'undefined' && "serviceWorker" in navigator && "storage" in navigator;
 }
